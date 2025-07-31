a026515c9271781cc09f930b3b68a5974bd11294
Sanand Tripathi <sarangpani2025@gmail.com>
2025-07-31 11:11:19 +0530

    Update 125_valid_palindrome.cpp

--- 125_valid_palindrome.cpp
+++ 125_valid_palindrome.cpp
@@ -1,7 +1,8 @@
 /*First used a for loop for converting all capital letters to small letters, 
 then another for loop for pushing all alphabets & numbers into another string, then used 
 the last for loop to see if the element at 'i'th position mathches the element at 'n-i-1'th position
-or not (basically condition of a palindrome). Finally put the condition that if it does not match then return false, otherwise return true.*/
+or not (basically condition of a palindrome). Finally put the condition that if it does not match then return false, otherwise return true.
+This is very beginner freindly approach, without using any pointers or any tough data structures*/
 class Solution {
 public:
     bool isPalindrome(string s) {
